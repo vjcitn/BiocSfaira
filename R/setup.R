9368d9800cfd096b143fae303646510f5fa03169
vjcitn <stvjc@channing.harvard.edu>
2023-07-11 08:21:14 -0400

    adding vignette

--- R/setup.R
+++ R/setup.R
@@ -1,10 +1,31 @@
+#' hand reference to ui back to R
+#' @export
+sfaira_ui = function() {
+  sfaira = reticulate::import("sfaira")
+  sfaira$ui$UserInterface(sfaira_repo=TRUE)
+}
 
 #' look at list of embeddings
 #' @import reticulate
 #' @export
 list_embedding_models = function() {
-  sfaira = import("sfaira")
-  ui = sfaira$ui$UserInterface(sfaira_repo=True)
+  sfaira = reticulate::import("sfaira")
+  ui = sfaira$ui$UserInterface(sfaira_repo=TRUE)
   ui$zoo_embedding$available_model_ids
 }
 
+#' look at list of celltypers
+#' @import reticulate
+#' @export
+list_celltype_models = function() {
+  sfaira = reticulate::import("sfaira")
+  ui = sfaira$ui$UserInterface(sfaira_repo=TRUE)
+  ui$zoo_celltype$available_model_ids
+}
+
+#' scanpy reference
+#' @export
+scanpy_ref = function() {
+ sc = reticulate::import("scanpy")
+ sc
+}
