267a6519529833cf48c21ed7c2548934617438ca
vjcitn <stvjc@channing.harvard.edu>
2023-09-15 07:52:07 -0400

    kill off ui_old

--- R/basilisk.R
+++ R/basilisk.R
@@ -3,6 +3,7 @@
 bsklenv <- basilisk::BasiliskEnvironment(
   envname = "bsklenv", packages = "pandas==1.3.5",
   pkgname = "BiocSfaira", pip = c("tensorflow==2.8.0",
-    "numpy==1.21.5", "sfaira==0.3.12", "protobuf==3.20.0"))
+    "numpy==1.21.5", "sfaira==0.3.12", "protobuf==3.20.0",
+    "scanpy==1.9.5"))
 
 
